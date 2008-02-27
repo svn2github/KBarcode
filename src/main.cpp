22fb67179c9bf66c510f6dd16e4f216face1f1c4
vzsolt <vzsolt@5afad502-fdf2-44c4-982b-b425be14779c>
2008-02-27 19:31:52 +0000

    Removed the I18N_NOOP macros, updated the KCmdLineArgs and KAboutData stuff to the KDE4 way.
    git-svn-id: https://svn.code.sf.net/p/kbarcode/code/trunk@18 5afad502-fdf2-44c4-982b-b425be14779c

--- src/main.cpp
+++ src/main.cpp
@@ -30,26 +30,6 @@
     #define VERSION "Unknown Version"
 #endif
 
-static KCmdLineOptions options[] =
-{
-    { "+[file]", I18N_NOOP("Load the file in KBarcode (if --batch is specified, the file will be used in batchprinting mode)"), 0},
-    { "print", I18N_NOOP("Print any loaded files immediately on the default printer "
-                         "or on the printer specified by the --printer commandline option and exit afterwards"), 0 },
-    { "printer <printer>", I18N_NOOP("Printer/destination to print on"), 0},
-    { ":", I18N_NOOP("KBarcode Modes:"), 0 },
-    { "barcode", I18N_NOOP("Start KBarcode as xbarcode replacement"), 0 },
-    { "label", I18N_NOOP("Open the label editor"), 0 },
-    { "batch", I18N_NOOP("Start directly in batchprinting mode"), 0 },
-    { ":", I18N_NOOP("Additional options for batchprinting (--batch):"), 0 },
-    { "output <mode>", I18N_NOOP("Output format where mode is one of POSTSCRIPT|IMAGE|BARCODE"), "POSTSCRIPT" },
-    { "serialnumber <value>", I18N_NOOP("Set the serial number to be used"), 0 },
-    { "serialinc <value>", I18N_NOOP("Increase a previously specified serial number using this value for each printed label"), 0 },
-    { "numlabels <value>", I18N_NOOP("Print <value> labels without any data"), 0 },
-    { "importsql <query>", I18N_NOOP("Import variable data from a specified sql query"), 0 },
-    { "importcsv <csvfile>", I18N_NOOP("Import variable data from a specified csv file"), 0 },
-    KCmdLineLastOption
-};
-
 void setupDirs()
 {
     if( locate("data", "kbarcode/logo.png").isEmpty() ) {
@@ -72,37 +52,57 @@ void setupDirs()
 
 int main(int argc, char *argv[])
 {
-    KAboutData aboutData( "kbarcode", I18N_NOOP("KBarcode"), VERSION, I18N_NOOP(
-        "KBarcode is a barcode and label printing application for KDE 3."
-        ), KAboutData::License_GPL, "KBarcode Build:\n" __DATE__ " " __TIME__
-        , "(c) 2001-2006, Dominik Seichter\n\n",
+    KAboutData aboutData( "kbarcode", "kbarcode", ki18n("KBarcode"), VERSION,
+            ki18n("KBarcode4 is a barcode and label printing application for KDE 4."),
+            KAboutData::License_GPL, ki18n("(c) 2001-2008, Dominik Seichter"), KLocalizedString()
         "http://www.kbarcode.net", "kbarcode-users@lists.sourceforge.net" );
 
-    aboutData.addAuthor("Dominik Seichter", I18N_NOOP("Programmer"), "domseichter@web.de");
-    aboutData.addAuthor("Stefan \"Stonki\" Onken",
-        I18N_NOOP("Project Manager"),
+    aboutData.addAuthor(ki18n("Dominik Seichter"), ki18n("Programmer"), "domseichter@web.de");
+    aboutData.addAuthor(ki18n("Stefan \"Stonki\" Onken"),
+        ki18n("Project Manager"),
         "support@stonki.de", "http://www.stonki.de" );
+    aboutData.addAuthor(ki18n("Váradi Zsolt Gyula"), ki18n("KBarcode4 Developer"), "vzsolt@users.sourceforge.net");
 
-    aboutData.addCredit("Alessandro Rubini", I18N_NOOP("Wrote GNU Barcode on which kbarcode is based."),
+    aboutData.addCredit(ki18n("Alessandro Rubini"), ki18n("Wrote GNU Barcode on which kbarcode is based."),
         "rubini@prosa.it", "http://arcana.linux.it/" );
-    aboutData.addCredit("Terry Burton", I18N_NOOP("Author of Barcode Writer in Pure Postscript"),
+    aboutData.addCredit(ki18n("Terry Burton"), ki18n("Author of Barcode Writer in Pure Postscript"),
                         "tez@terryburton.co.uk", "http://www.terryburton.co.uk/" );
-    aboutData.addCredit("Daniele Medri", I18N_NOOP("Italian translation"), "madrid@linuxmeeting.net" );
-    aboutData.addCredit("Anton Vaaranmaa",
-         I18N_NOOP("Finnish and Swedish translation, created the KBarcode icon"), "antonv@postikaista.net" );
-    aboutData.addCredit("Miguel Revilla Rodr�uez", I18N_NOOP("Spanish translation"), "yo@miguelrevilla.com" );
-    aboutData.addCredit("Sandor Jager", I18N_NOOP("Hungarian translation"), "jager@puskas.hu" );
-    aboutData.addCredit("Daniel Etzold",
-        I18N_NOOP("Tab icon code stolen from his excellent app qtella."), "detzold@qtella.net" );
-    aboutData.addCredit("Richard J. Moore", I18N_NOOP("Wrote the RichText KPart"), "rich@kde.org", "http://xmelegance.org/" );
-    aboutData.addCredit("Yann Bouan", I18N_NOOP("French Translation"), "yann@bouan.net" );
-    aboutData.addCredit("Erich Kitzmüller", I18N_NOOP("Help with ZPL and IPL code"), "kitzmueller@metasyst.at" );
-    aboutData.addCredit("Dag Nygren", I18N_NOOP("Wrote many patches to improve KBarcode"), "dag@newtech.fi" );
-    aboutData.addCredit("Frank Schoolmeesters", I18N_NOOP("Made the Netherlands translation"), "frank_schoolmeesters@fastmail.fm" );
-    aboutData.addCredit("John Volpe", I18N_NOOP("Added lot's of useful data fields to kbarcode"), "jtvolpe@cape.com" );
-    aboutData.addCredit("Nyssa s.r.l.", I18N_NOOP("Added TEC barcode printer support"), "imorrison@nyssa.com.ar", "http://www.nyssa.com.ar" );
-    aboutData.addCredit("Brian Glass", I18N_NOOP("Added EPCL barcode printer support"), "brian@glassbrian.com", "http://www.glassbrian.com" );
+    aboutData.addCredit(ki18n("Daniele Medri"), ki18n("Italian translation"), "madrid@linuxmeeting.net" );
+    aboutData.addCredit(ki18n("Anton Vaaranmaa"),
+         ki18n("Finnish and Swedish translation, created the KBarcode icon"), "antonv@postikaista.net" );
+    aboutData.addCredit(ki18n("Miguel Revilla Rodr�uez"), ki18n("Spanish translation"), "yo@miguelrevilla.com" );
+    aboutData.addCredit(ki18n("Sandor Jager"), ki18n("Hungarian translation"), "jager@puskas.hu" );
+    aboutData.addCredit(ki18n("Daniel Etzold"),
+        ki18n("Tab icon code stolen from his excellent app qtella."), "detzold@qtella.net" );
+    aboutData.addCredit(ki18n("Richard J. Moore"), ki18n("Wrote the RichText KPart"), "rich@kde.org", "http://xmelegance.org/" );
+    aboutData.addCredit(ki18n("Yann Bouan"), ki18n("French Translation"), "yann@bouan.net" );
+    aboutData.addCredit(ki18n("Erich Kitzmüller"), ki18n("Help with ZPL and IPL code"), "kitzmueller@metasyst.at" );
+    aboutData.addCredit(ki18n("Dag Nygren"), ki18n("Wrote many patches to improve KBarcode"), "dag@newtech.fi" );
+    aboutData.addCredit(ki18n("Frank Schoolmeesters"), ki18n("Made the Netherlands translation"), "frank_schoolmeesters@fastmail.fm" );
+    aboutData.addCredit(ki18n("John Volpe"), ki18n("Added lot's of useful data fields to kbarcode"), "jtvolpe@cape.com" );
+    aboutData.addCredit(ki18n("Nyssa s.r.l."), ki18n("Added TEC barcode printer support"), "imorrison@nyssa.com.ar", "http://www.nyssa.com.ar" );
+    aboutData.addCredit(ki18n("Brian Glass"), ki18n("Added EPCL barcode printer support"), "brian@glassbrian.com", "http://www.glassbrian.com" );
+
     KCmdLineArgs::init( argc, argv, &aboutData );
+    
+    KCmdLineOptions options;
+
+    options.add( "+[file]", ki18n("Load the file in KBarcode (if --batch is specified, the file will be used in batchprinting mode)"), 0);
+    options.add( "print", ki18n("Print any loaded files immediately on the default printer "
+            "or on the printer specified by the --printer commandline option and exit afterwards"), 0 );
+    options.add( "printer <printer>", ki18n("Printer/destination to print on"), 0);
+    options.add( ":", ki18n("KBarcode Modes:"), 0 );
+    options.add( "barcode", ki18n("Start KBarcode as xbarcode replacement"), 0 );
+    options.add( "label", ki18n("Open the label editor"), 0 );
+    options.add( "batch", ki18n("Start directly in batchprinting mode"), 0 );
+    options.add( ":", ki18n("Additional options for batchprinting (--batch):"), 0 );
+    options.add( "output <mode>", ki18n("Output format where mode is one of POSTSCRIPT|IMAGE|BARCODE"), "POSTSCRIPT" );
+    options.add( "serialnumber <value>", ki18n("Set the serial number to be used"), 0 );
+    options.add( "serialinc <value>", ki18n("Increase a previously specified serial number using this value for each printed label"), 0 );
+    options.add( "numlabels <value>", ki18n("Print <value> labels without any data"), 0 );
+    options.add( "importsql <query>", ki18n("Import variable data from a specified sql query"), 0 );
+    options.add( "importcsv <csvfile>", ki18n("Import variable data from a specified csv file"), 0 );
+
     KCmdLineArgs::addCmdLineOptions( options );
 
     KApplication a;
